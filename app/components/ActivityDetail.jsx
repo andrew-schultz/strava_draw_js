068fca84fa70c9b3ef01d0f23ca39f29e444f7e6
Andrew Schultz <as173171@gmail.com>
2024-11-09 13:19:06 -0500

    fix build, clean up a bit

--- app/components/ActivityDetail.jsx
+++ app/components/ActivityDetail.jsx
@@ -1,50 +1,19 @@
 "use client";
-import { useEffect, useState, useMemo, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 var polyline = require('@mapbox/polyline');
 
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-
-import { MapContainer, Marker, TileLayer, Tooltip, Polyline } from "react-leaflet"
-
+// import 'leaflet/dist/leaflet.css';
 
 const ActivityDetail = ({activity}) => {
-    const ref = useRef();
     const [polylines, setPolylines] = useState();
-    const [map, setMap] = useState();
 
     useEffect(() => {
         let polylines = activity.map.summary_polyline;
         let decoded_polylines = polyline.decode(polylines);
         setPolylines(decoded_polylines)
-        // let map
-        // if (!map) {
-        //     // map = leaflet.map('map', {center: decoded_polylines[0], zoom: 12, zoomControl: false, tiles: null})
-        //     debugger
-        //     var firstpolyline = new leaflet.Polyline(decoded_polylines, {
-        //         color: 'black',
-        //         weight: 3,
-        //         opacity: 0.5,
-        //         smoothFactor: 1
-        //     });
-        //     firstpolyline.addTo(map);
-        //     debugger
-        // }
-            
-        // let map = <LazyMap position={decoded_polylines[0]} zoom={12} ref={ref}/> 
-        // debugger
-
-        // setMap(map)
-        // MapContainer.map.
     }, [])
-
-    const LazyMap = dynamic(() => import('./LeafletMap'), { 
-        loading: () => <p>A map is loading</p>,
-        ssr: false
-    })
-    
 
     const MapComponent = dynamic(() => import('./LeafletMap'), {
         ssr: false,
--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,17 +1,10 @@
-'use client'
-import dynamic from 'next/dynamic'
 import HomeMain from '../components/HomeMain'
- 
-const DynamicComponentWithNoSSR = dynamic(
-    () => import('../components/HomeMain'),
-    { ssr: false }
-)
  
 export default function Page({stravaCookies}) {
   return (
     <div>
-        <DynamicComponentWithNoSSR stravaCookies={null} />
-        {/* <HomeMain stravaCookies={null}></HomeMain> */}
+        {/* <DynamicComponentWithNoSSR stravaCookies={null} /> */}
+        <HomeMain stravaCookies={null}></HomeMain>
     </div>
   )
 }
